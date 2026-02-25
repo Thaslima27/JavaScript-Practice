03ba8a3d4bc7077a4777a16435001745c227e764
Thasli <thaslinazeer27@gmail.com>
2026-02-25 12:06:10 +0530

    Added objectfreeze

--- ques3.js
+++ ques3.js
@@ -13,3 +13,10 @@ let user2={...user1};
 user2.age=24;
 console.log("Original:",user1);
 console.log("Cloned:",user2);
+
+//objectfreeze()
+let user={name:"thasli",age:21};
+Object.freeze(user);//it freeze the user
+user.age=24;//won't modify
+delete user.name;//won't delete
+console.log(user);
