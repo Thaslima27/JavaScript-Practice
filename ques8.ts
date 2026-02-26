e84c802aa7dea371e483612fe690287af50f00c8
Thasli <thaslinazeer27@gmail.com>
2026-02-26 11:11:40 +0530

    typescript prg added

---
+++ ques8.ts
@@ -0,0 +1,39 @@
+//add two numbers
+function addNumbers(a:number, b:number):number {
+return a + b;
+}
+let result = addNumbers(10, 20);
+console.log(result);
+
+//Interface
+interface User {
+id: number;
+name: string;
+email: string;
+}
+let user1: User = {
+id: 1,
+name: "Thasli",
+email: "thasli@email.com"
+};
+console.log(user1);
+
+//Enum
+enum Direction {
+Up,
+Down,
+Left,
+Right
+}
+let move: Direction = Direction.Up;
+console.log(move);
+
+//optional parameters
+function greet(name:string,age?:number):string {
+if(age) {
+return `Hello ${name}, you are ${age} years old`;
+}
+return `Hello ${name}`;
+}
+console.log(greet("Zara"));
+console.log(greet("Zara", 22));
